456605c5ec37fe57052f4ab51f4dcfb907124ebe
nori <nohriter@gmail.com>
2024-06-14 15:32:43 +0900

    [level 0] Title: 이어 붙인 수, Time: 15.14 ms, Memory: 74.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        String odd = "";
+        String even = "";
+        
+        for(int i=0; i<num_list.length; i++) {
+            if(num_list[i] % 2 != 0) {
+                odd += num_list[i];
+            } else {
+                even += num_list[i];
+            }
+        }        
+        return Integer.parseInt(odd) + Integer.parseInt(even);
+    }
+}
