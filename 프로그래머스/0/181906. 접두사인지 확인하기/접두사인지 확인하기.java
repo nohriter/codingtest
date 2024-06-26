0420568c9a39430bb94ad1d661c7dd7d73f28520
nori <nohriter@gmail.com>
2024-06-26 10:07:10 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.java
@@ -1,11 +1,5 @@
 class Solution {
     public int solution(String my_string, String is_prefix) {
-        int answer = 0;
-        
-        if(my_string.startsWith(is_prefix)) {
-            answer++;
-        }
-        
-        return answer;
+        return my_string.startsWith(is_prefix) ? 1 : 0;
     }
 }
