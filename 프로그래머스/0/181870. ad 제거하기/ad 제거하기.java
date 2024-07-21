f3291c1e915d0938c105cb8ca5894b1e703d6756
nori <nohriter@gmail.com>
2024-07-21 22:02:30 +0900

    [level 0] Title: ad 제거하기, Time: 0.96 ms, Memory: 84.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strArr) {
+        List<String> resultList = new ArrayList<>();
+        
+        for (String str : strArr) {
+            if (!str.contains("ad")) {
+                resultList.add(str);
+            }
+        }
+        
+        return resultList.toArray(new String[0]);
+    }
+}
