7a875e13c954e43f17946420b7bd59cf63da17b5
nori <nohriter@gmail.com>
2024-08-11 11:56:28 +0900

    [level 0] Title: l로 만들기, Time: 8.54 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : myString.toCharArray()) {
+            if (c < 'l') {
+                result.append('l');
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+}
